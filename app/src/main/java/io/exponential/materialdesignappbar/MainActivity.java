1c1443ef6bd83b6cd51c8639cc724b200e0fbe57
akbarahmed <akbar501@gmail.com>
2015-07-09 00:19:11 -0700

    set Toolbar as ActionBar

--- app/src/main/java/io/exponential/materialdesignappbar/MainActivity.java
+++ app/src/main/java/io/exponential/materialdesignappbar/MainActivity.java
@@ -1,7 +1,9 @@
 package io.exponential.materialdesignappbar;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +14,25 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Call
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+            .beginTransaction()
+            .add(R.id.container, MainFragment.newInstance("Placeholder"))
+            .commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
